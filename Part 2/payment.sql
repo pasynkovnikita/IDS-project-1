957024443efebde09245b2d62bbe83b6fdc50821
Lena <xmaroc00@stud.fit.vutbr.cz>
2023-03-24 21:14:04 +0100

    feat: creates payment table

---
+++ Part 2/payment.sql
@@ -0,0 +1,18 @@
+
+DROP TABLE payment;
+
+CREATE TABLE payment
+(
+    payment_id   INT PRIMARY KEY,
+    order_id     INT   NOT NULL,
+    user_id      INT   NOT NULL,
+    sum          FLOAT NOT NULL, -- TODO: calculate sum from order
+    payment_date DATE  NOT NULL,
+    CONSTRAINT FK_payment_order FOREIGN KEY (order_id) REFERENCES "order",
+    CONSTRAINT FK_payment_user FOREIGN KEY (user_id) REFERENCES registered_user
+);
+
+INSERT INTO payment (payment_id, order_id, user_id, sum, payment_date)
+VALUES (1, 1, 1, 100.90, '10.10.2010');
+
+SELECT * FROM payment;
