97a328667abe953b4524c62b3520d9c9fc983f93
Lena <xmaroc00@stud.fit.vutbr.cz>
2023-03-24 21:12:33 +0100

    refactor: formats code

--- Part 2/product.sql
+++ Part 2/product.sql
@@ -3,26 +3,28 @@ DROP TABLE category;
 
 CREATE TABLE category
 (
-    category_id INT PRIMARY KEY,
+    category_id   INT PRIMARY KEY,
     category_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE product
 (
-    product_id INT PRIMARY KEY,
-    category_id INT NOT NULL,
-    product_name VARCHAR(50) NOT NULL,
-    product_price FLOAT NOT NULL,
-    product_count INT NOT NULL,
+    product_id    INT PRIMARY KEY,
+    category_id   INT         NOT NULL,
+    product_name  VARCHAR(50) NOT NULL,
+    product_price FLOAT       NOT NULL,
+    product_count INT         NOT NULL,
     CONSTRAINT FK_product_category_id FOREIGN KEY (category_id) REFERENCES category
 );
 
 INSERT INTO category(category_id, category_name)
-VALUES(1, 'Foreign books');
+VALUES (1, 'Foreign books');
 
 INSERT INTO product(product_id, category_id, product_name, product_price, product_count)
-VALUES(1, 1, 'The Little Prince', 10.00, 100);
+VALUES (1, 1, 'The Little Prince', 10.00, 100);
 
-SELECT * FROM CATEGORY;
-SELECT * FROM PRODUCT;
+SELECT *
+FROM CATEGORY;
+SELECT *
+FROM PRODUCT;
 
