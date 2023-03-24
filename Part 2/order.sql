7bb87b258a2bce325b456e8d781d63a35b963ea6
pasynkovnikita <pasynkov.n.d@gmail.com>
2023-03-24 19:50:37 +0100

    feat: create table for order

---
+++ Part 2/order.sql
@@ -0,0 +1,17 @@
+DROP TABLE "order";
+
+CREATE TABLE "order"
+(
+    order_id   INT          NOT NULL PRIMARY KEY,
+    address    VARCHAR(255) NOT NULL,
+    status     VARCHAR(20)  NOT NULL,
+    order_date DATE         NOT NULL,
+    user_id    INT          NOT NULL,
+    CONSTRAINT FK_order_user_id FOREIGN KEY (user_id) REFERENCES XPASYN00.REGISTERED_USER
+);
+
+
+INSERT INTO "order" (order_id, address, status, order_date, user_id)
+VALUES (1, '123 Main St', 'pending', '01.01.2019', 1);
+
+SELECT * FROM "order";
