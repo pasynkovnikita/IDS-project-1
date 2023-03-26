be2d3eef193e739cecda6d52c0d0ed96c00bc662
pasynkovnikita <pasynkov.n.d@gmail.com>
2023-03-26 19:06:14 +0200

    feat: add check for phone number

--- Part 2/user.sql
+++ Part 2/user.sql
@@ -2,15 +2,16 @@ DROP TABLE registered_user;
 
 CREATE TABLE registered_user
 (
-    user_id      INT          NOT NULL PRIMARY KEY,
-    login        VARCHAR(20)  NOT NULL,
-    password     VARCHAR(20)  NOT NULL,
-    first_name   VARCHAR(20)  NOT NULL,
-    last_name    VARCHAR(20)  NOT NULL,
-    email        VARCHAR(20)  NOT NULL,
-    phone_number VARCHAR(20)  NOT NULL,
-    address      VARCHAR(256) NOT NULL,
-    CONSTRAINT length_password CHECK (length(password) between 8 and 20)
+    user_id      INT GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    login        VARCHAR(20)                      NOT NULL,
+    password     VARCHAR(20)                      NOT NULL,
+    first_name   VARCHAR(20)                      NOT NULL,
+    last_name    VARCHAR(20)                      NOT NULL,
+    email        VARCHAR(20)                      NOT NULL,
+    phone_number VARCHAR(20)                      NOT NULL,
+    address      VARCHAR(256)                     NOT NULL,
+    CONSTRAINT length_password CHECK (length(password) between 8 and 20),
+    CONSTRAINT phone_number_check CHECK (REGEXP_LIKE(phone_number, '^\+?[0-9]{11,13}$'))
 );
 
 INSERT INTO registered_user (user_id, login, password, first_name, last_name, email, phone_number, address)
