7a71e875d6994529650b1da1fd593043dfc4658b
Lena <xmaroc00@stud.fit.vutbr.cz>
2023-03-26 20:38:31 +0200

    feat: adds password number checking

--- Part 2/user.sql
+++ Part 2/user.sql
@@ -9,10 +9,11 @@ CREATE TABLE registered_user
     last_name    VARCHAR(20)  NOT NULL,
     email        VARCHAR(20)  NOT NULL,
     phone_number VARCHAR(20)  NOT NULL,
-    address      VARCHAR(256) NOT NULL
+    address      VARCHAR(256) NOT NULL,
+    CONSTRAINT length_password CHECK (length(password) between 8 and 20)
 );
 
 INSERT INTO registered_user (user_id, login, password, first_name, last_name, email, phone_number, address)
-VALUES (1, 'user1', 'password1', 'John', 'Doe', 'xqwerty00@gmail.com', '1234567890', '123 Main St, New York, NY 10001');
+VALUES (1, 'user1', 'password1', 'John', 'Doe', 'xqwerty00@gmail.com', '79999210755', '123 Main St, New York, NY 10001');
 
 SELECT * FROM registered_user;
